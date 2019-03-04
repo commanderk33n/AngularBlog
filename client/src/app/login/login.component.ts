e0e441de076f037ffdfa4e07f79e876599b31014
commanderk33n <torsten.m85@gmail.com>
2019-03-05 00:38:21 +0100

    added homecomponent and login process

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
 import { User } from '../models/user.model';
+import { Router } from '@angular/router';
  
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent {
  
   public user : User;
  
-  constructor(private loginService: LoginService) {
+  constructor(private loginService: LoginService, private router: Router) {
       this.user = new User();
   }
  
@@ -20,6 +21,12 @@ export class LoginComponent {
     if(this.user.username && this.user.password) {
         this.loginService.validateLogin(this.user).subscribe(result => {
         console.log('result is ', result);
+        if(result['status'] === 'success') {
+          this.router.navigate(['/home']);
+        } else {
+          alert('Wrong username password');
+        }
+         
       }, error => {
         console.log('error is ', error);
       });
--- server/app.js
+++ server/app.js
@@ -3,14 +3,33 @@ const bodyParser = require('body-parser')
 const app = express()
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost/blogDb';
+const User = require('./model/user');
  
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false}))
+
+mongoose.set('useCreateIndex', true);
  
 app.post('/api/user/login', (req, res) => {
-    mongoose.connect(url, { useNewUrlParser: true },  function(err){
+    mongoose.connect(url, { useNewUrlParser: true }, function(err){
         if(err) throw err;
-        console.log('connected successfully, username is ',req.body.username,' password is ',req.body.password);
+        User.find({
+            username : req.body.username, password : req.body.password
+        }, function(err, user){
+            if(err) throw err;
+            if(user.length === 1){  
+                return res.status(200).json({
+                    status: 'success',
+                    data: user
+                })
+            } else {
+                return res.status(200).json({
+                    status: 'fail',
+                    message: 'Login Failed'
+                })
+            }
+             
+        })
     });
 })
  
